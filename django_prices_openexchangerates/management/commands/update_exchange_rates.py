a4d3f3ed8e2bed773fcc7a0a489131c5669b217c
Artur Smęt <artur.smet@mirumee.com>
2015-01-27 17:42:21 +0100

    Add management command for conversion rates import

---
+++ django_prices_openexchangerates/management/commands/update_exchange_rates.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from django.core.management.base import BaseCommand
+from ...tasks import update_conversion_rates
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        for conversion_rate in update_conversion_rates():
+            self.stdout.write(str(conversion_rate))
---
+++ django_prices_openexchangerates/tasks.py
@@ -0,0 +1,45 @@
+from __future__ import division
+from __future__ import unicode_literals
+
+from decimal import Decimal
+
+import requests
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from .models import ConversionRate
+
+BASE_URL = r'http://openexchangerates.org/api'
+ENDPOINT_LATEST = BASE_URL + r'/latest.json'
+try:
+    API_KEY = settings.OPENEXCHANGERATES_API_KEY
+except AttributeError:
+    raise ImproperlyConfigured('OPENEXCHANGERATES_API_KEY is required')
+
+
+class ExchangeRates(object):
+
+    def __init__(self, rates, default_currency=None):
+        self.rates = rates
+        self.default_currency = (
+            default_currency or settings.DEFAULT_CURRENCY)
+
+    def __getitem__(self, item):
+        rate = self.rates[item]
+        return rate / self.rates[self.default_currency]
+
+
+def get_latest_exchange_rates():
+    response = requests.get(ENDPOINT_LATEST, params={'app_id': API_KEY})
+    response.raise_for_status()
+    exchange_data = response.json(parse_int=Decimal, parse_float=Decimal)
+    return ExchangeRates(rates=exchange_data['rates'])
+
+
+def update_conversion_rates():
+    exchange_rates = get_latest_exchange_rates()
+    conversion_rates = ConversionRate.objects.all()
+    for conversion_rate in conversion_rates:
+        new_exchange_rate = exchange_rates[conversion_rate.to_currency]
+        conversion_rate.rate = new_exchange_rate
+        conversion_rate.save()
+    return conversion_rates
