37a3d99bd3fb2081f7b972c0252df4d8873eb814
Artur Smęt <artur.smet@mirumee.com>
2015-01-28 11:32:08 +0100

    Add templatetags

---
+++ django_prices_openexchangerates/templatetags/currency_conversion.py
@@ -0,0 +1,25 @@
+from django.template import Library
+from django_prices.templatetags import prices_i18n
+
+from ..utils import exchange_currency
+
+
+register = Library()
+
+
+@register.simple_tag
+def gross(price, currency):
+    converted_price = exchange_currency(price, currency)
+    return prices_i18n.gross(converted_price)
+
+
+@register.simple_tag
+def net(price, currency):
+    converted_price = exchange_currency(price, currency)
+    return prices_i18n.net(converted_price)
+
+
+@register.simple_tag
+def tax(price, currency):
+    converted_price = exchange_currency(price, currency)
+    return prices_i18n.tax(converted_price)
