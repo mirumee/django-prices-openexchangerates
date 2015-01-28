6e0d583e0c3eea7ca9e7a37567cfc7535d8f406b
Artur Smęt <artur.smet@mirumee.com>
2015-01-28 17:56:25 +0100

    Make rates parsing more readable

--- django_prices_openexchangerates/tasks.py
+++ django_prices_openexchangerates/tasks.py
@@ -17,30 +17,23 @@ except AttributeError:
     raise ImproperlyConfigured('OPENEXCHANGERATES_API_KEY is required')
 
 
-class ExchangeRates(object):
-
-    def __init__(self, rates, default_currency=None):
-        self.rates = rates
-        self.default_currency = (
-            default_currency or settings.DEFAULT_CURRENCY)
-
-    def __getitem__(self, item):
-        rate = self.rates[item]
-        return rate / self.rates[self.default_currency]
+def extract_rate(rates, currency):
+    base_rate = rates[settings.DEFAULT_CURRENCY]
+    return rates[currency] / base_rate
 
 
 def get_latest_exchange_rates():
     response = requests.get(ENDPOINT_LATEST, params={'app_id': API_KEY})
     response.raise_for_status()
-    exchange_data = response.json(parse_int=Decimal, parse_float=Decimal)
-    return ExchangeRates(rates=exchange_data['rates'])
+    return response.json(parse_int=Decimal, parse_float=Decimal)
 
 
 def update_conversion_rates():
     exchange_rates = get_latest_exchange_rates()
     conversion_rates = ConversionRate.objects.all()
     for conversion_rate in conversion_rates:
-        new_exchange_rate = exchange_rates[conversion_rate.to_currency]
+        new_exchange_rate = extract_rate(exchange_rates,
+                                         conversion_rate.to_currency)
         conversion_rate.rate = new_exchange_rate
         conversion_rate.save(update_fields=['rate'])
     return conversion_rates
