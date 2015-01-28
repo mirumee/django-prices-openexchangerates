7a898de6dc904f534072667b880ff923ced0020e
Artur Smęt <artur.smet@mirumee.com>
2015-01-28 11:32:23 +0100

    Reformat currency list

--- django_prices_openexchangerates/currencies.py
+++ django_prices_openexchangerates/currencies.py
@@ -1,342 +1,175 @@
 from __future__ import unicode_literals
 
-'''
-List of standard currencies available as ISO 4217. This list was generated
-at 14th Feb 2014 based on the ISO standard at the time. Not guarantee on
-the correctness or completeness of this list.
-'''
-from django.utils.translation import ugettext_lazy as _
-
-
-AED = 'AED'
-AFN = 'AFN'
-ALL = 'ALL'
-AMD = 'AMD'
-ANG = 'ANG'
-AOA = 'AOA'
-ARS = 'ARS'
-AUD = 'AUD'
-AWG = 'AWG'
-AZN = 'AZN'
-BAM = 'BAM'
-BBD = 'BBD'
-BDT = 'BDT'
-BGN = 'BGN'
-BHD = 'BHD'
-BIF = 'BIF'
-BMD = 'BMD'
-BND = 'BND'
-BOB = 'BOB'
-BRL = 'BRL'
-BSD = 'BSD'
-BTN = 'BTN'
-BWP = 'BWP'
-BYR = 'BYR'
-BZD = 'BZD'
-CAD = 'CAD'
-CDF = 'CDF'
-CHF = 'CHF'
-CLP = 'CLP'
-CNY = 'CNY'
-COP = 'COP'
-CRC = 'CRC'
-CUC = 'CUC'
-CUP = 'CUP'
-CVE = 'CVE'
-CZK = 'CZK'
-DJF = 'DJF'
-DKK = 'DKK'
-DOP = 'DOP'
-DZD = 'DZD'
-EGP = 'EGP'
-ERN = 'ERN'
-ETB = 'ETB'
-EUR = 'EUR'
-FJD = 'FJD'
-FKP = 'FKP'
-GBP = 'GBP'
-GEL = 'GEL'
-GGP = 'GGP'
-GHS = 'GHS'
-GIP = 'GIP'
-GMD = 'GMD'
-GNF = 'GNF'
-GTQ = 'GTQ'
-GYD = 'GYD'
-HKD = 'HKD'
-HNL = 'HNL'
-HRK = 'HRK'
-HTG = 'HTG'
-HUF = 'HUF'
-IDR = 'IDR'
-ILS = 'ILS'
-IMP = 'IMP'
-INR = 'INR'
-IQD = 'IQD'
-IRR = 'IRR'
-ISK = 'ISK'
-JEP = 'JEP'
-JMD = 'JMD'
-JOD = 'JOD'
-JPY = 'JPY'
-KES = 'KES'
-KGS = 'KGS'
-KHR = 'KHR'
-KMF = 'KMF'
-KPW = 'KPW'
-KRW = 'KRW'
-KWD = 'KWD'
-KYD = 'KYD'
-KZT = 'KZT'
-LAK = 'LAK'
-LBP = 'LBP'
-LKR = 'LKR'
-LRD = 'LRD'
-LSL = 'LSL'
-LTL = 'LTL'
-LVL = 'LVL'
-LYD = 'LYD'
-MAD = 'MAD'
-MDL = 'MDL'
-MGA = 'MGA'
-MKD = 'MKD'
-MMK = 'MMK'
-MNT = 'MNT'
-MOP = 'MOP'
-MRO = 'MRO'
-MUR = 'MUR'
-MVR = 'MVR'
-MWK = 'MWK'
-MXN = 'MXN'
-MYR = 'MYR'
-MZN = 'MZN'
-NAD = 'NAD'
-NGN = 'NGN'
-NIO = 'NIO'
-NOK = 'NOK'
-NPR = 'NPR'
-NZD = 'NZD'
-OMR = 'OMR'
-PAB = 'PAB'
-PEN = 'PEN'
-PGK = 'PGK'
-PHP = 'PHP'
-PKR = 'PKR'
-PLN = 'PLN'
-PYG = 'PYG'
-QAR = 'QAR'
-RON = 'RON'
-RSD = 'RSD'
-RUB = 'RUB'
-RWF = 'RWF'
-SAR = 'SAR'
-SBD = 'SBD'
-SCR = 'SCR'
-SDG = 'SDG'
-SEK = 'SEK'
-SGD = 'SGD'
-SHP = 'SHP'
-SLL = 'SLL'
-SOS = 'SOS'
-SPL = 'SPL'
-SRD = 'SRD'
-STD = 'STD'
-SVC = 'SVC'
-SYP = 'SYP'
-SZL = 'SZL'
-THB = 'THB'
-TJS = 'TJS'
-TMT = 'TMT'
-TND = 'TND'
-TOP = 'TOP'
-TRY = 'TRY'
-TTD = 'TTD'
-TVD = 'TVD'
-TWD = 'TWD'
-TZS = 'TZS'
-UAH = 'UAH'
-UGX = 'UGX'
-USD = 'USD'
-UYU = 'UY'
-UZS = 'UZS'
-VEF = 'VEF'
-VND = 'VND'
-VUV = 'VUV'
-WST = 'WST'
-XAF = 'XAF'
-XCD = 'XCD'
-XDR = 'XDR'
-XOF = 'XOF'
-XPF = 'XPF'
-YER = 'YER'
-ZAR = 'ZAR'
-ZMW = 'ZMW'
-ZWD = 'ZWD'
+# List of currencies supported by OpenExchange
+# http://openexchangerates.org/currencies.json
 
 
 CURRENCIES = {
-    AED: _('United Arab Emirates Dirham'),
-    AFN: _('Afghanistan Afghani'),
-    ALL: _('Albania Lek'),
-    AMD: _('Armenia Dram'),
-    ANG: _('Netherlands Antilles Guilder'),
-    AOA: _('Angola Kwanza'),
-    ARS: _('Argentina Peso'),
-    AUD: _('Australia Dollar'),
-    AWG: _('Aruba Guilder'),
-    AZN: _('Azerbaijan New Manat'),
-    BAM: _('Bosnia and Herzegovina Convertible Marka'),
-    BBD: _('Barbados Dollar'),
-    BDT: _('Bangladesh Taka'),
-    BGN: _('Bulgaria Lev'),
-    BHD: _('Bahrain Dinar'),
-    BIF: _('Burundi Franc'),
-    BMD: _('Bermuda Dollar'),
-    BND: _('Brunei Darussalam Dollar'),
-    BOB: _('Bolivia Boliviano'),
-    BRL: _('Brazil Real'),
-    BSD: _('Bahamas Dollar'),
-    BTN: _('Bhutan Ngultrum'),
-    BWP: _('Botswana Pula'),
-    BYR: _('Belarus Ruble'),
-    BZD: _('Belize Dollar'),
-    CAD: _('Canada Dollar'),
-    CDF: _('Congo/Kinshasa Franc'),
-    CHF: _('Switzerland Franc'),
-    CLP: _('Chile Peso'),
-    CNY: _('China Yuan Renminbi'),
-    COP: _('Colombia Peso'),
-    CRC: _('Costa Rica Colon'),
-    CUC: _('Cuba Convertible Peso'),
-    CUP: _('Cuba Peso'),
-    CVE: _('Cape Verde Escudo'),
-    CZK: _('Czech Republic Koruna'),
-    DJF: _('Djibouti Franc'),
-    DKK: _('Denmark Krone'),
-    DOP: _('Dominican Republic Peso'),
-    DZD: _('Algeria Dinar'),
-    EGP: _('Egypt Pound'),
-    ERN: _('Eritrea Nakfa'),
-    ETB: _('Ethiopia Birr'),
-    EUR: _('Euro Member Countries'),
-    FJD: _('Fiji Dollar'),
-    FKP: _('Falkland Islands (Malvinas) Pound'),
-    GBP: _('United Kingdom Pound'),
-    GEL: _('Georgia Lari'),
-    GGP: _('Guernsey Pound'),
-    GHS: _('Ghana Cedi'),
-    GIP: _('Gibraltar Pound'),
-    GMD: _('Gambia Dalasi'),
-    GNF: _('Guinea Franc'),
-    GTQ: _('Guatemala Quetzal'),
-    GYD: _('Guyana Dollar'),
-    HKD: _('Hong Kong Dollar'),
-    HNL: _('Honduras Lempira'),
-    HRK: _('Croatia Kuna'),
-    HTG: _('Haiti Gourde'),
-    HUF: _('Hungary Forint'),
-    IDR: _('Indonesia Rupiah'),
-    ILS: _('Israel Shekel'),
-    IMP: _('Isle of Man Pound'),
-    INR: _('India Rupee'),
-    IQD: _('Iraq Dinar'),
-    IRR: _('Iran Rial'),
-    ISK: _('Iceland Krona'),
-    JEP: _('Jersey Pound'),
-    JMD: _('Jamaica Dollar'),
-    JOD: _('Jordan Dinar'),
-    JPY: _('Japan Yen'),
-    KES: _('Kenya Shilling'),
-    KGS: _('Kyrgyzstan Som'),
-    KHR: _('Cambodia Riel'),
-    KMF: _('Comoros Franc'),
-    KPW: _('Korea (North) Won'),
-    KRW: _('Korea (South) Won'),
-    KWD: _('Kuwait Dinar'),
-    KYD: _('Cayman Islands Dollar'),
-    KZT: _('Kazakhstan Tenge'),
-    LAK: _('Laos Kip'),
-    LBP: _('Lebanon Pound'),
-    LKR: _('Sri Lanka Rupee'),
-    LRD: _('Liberia Dollar'),
-    LSL: _('Lesotho Loti'),
-    LTL: _('Lithuania Litas'),
-    LVL: _('Latvia Lat'),
-    LYD: _('Libya Dinar'),
-    MAD: _('Morocco Dirham'),
-    MDL: _('Moldova Le'),
-    MGA: _('Madagascar Ariary'),
-    MKD: _('Macedonia Denar'),
-    MMK: _('Myanmar (Burma) Kyat'),
-    MNT: _('Mongolia Tughrik'),
-    MOP: _('Macau Pataca'),
-    MRO: _('Mauritania Ouguiya'),
-    MUR: _('Mauritius Rupee'),
-    MVR: _('Maldives (Maldive Islands) Rufiyaa'),
-    MWK: _('Malawi Kwacha'),
-    MXN: _('Mexico Peso'),
-    MYR: _('Malaysia Ringgit'),
-    MZN: _('Mozambique Metical'),
-    NAD: _('Namibia Dollar'),
-    NGN: _('Nigeria Naira'),
-    NIO: _('Nicaragua Cordoba'),
-    NOK: _('Norway Krone'),
-    NPR: _('Nepal Rupee'),
-    NZD: _('New Zealand Dollar'),
-    OMR: _('Oman Rial'),
-    PAB: _('Panama Balboa'),
-    PEN: _('Peru Nuevo Sol'),
-    PGK: _('Papua New Guinea Kina'),
-    PHP: _('Philippines Peso'),
-    PKR: _('Pakistan Rupee'),
-    PLN: _('Poland Zloty'),
-    PYG: _('Paraguay Guarani'),
-    QAR: _('Qatar Riyal'),
-    RON: _('Romania New Le'),
-    RSD: _('Serbia Dinar'),
-    RUB: _('Russia Ruble'),
-    RWF: _('Rwanda Franc'),
-    SAR: _('Saudi Arabia Riyal'),
-    SBD: _('Solomon Islands Dollar'),
-    SCR: _('Seychelles Rupee'),
-    SDG: _('Sudan Pound'),
-    SEK: _('Sweden Krona'),
-    SGD: _('Singapore Dollar'),
-    SHP: _('Saint Helena Pound'),
-    SLL: _('Sierra Leone Leone'),
-    SOS: _('Somalia Shilling'),
-    SPL: _('Seborga Luigino'),
-    SRD: _('Suriname Dollar'),
-    STD: _('São Tomé and Príncipe Dobra'),
-    SVC: _('El Salvador Colon'),
-    SYP: _('Syria Pound'),
-    SZL: _('Swaziland Lilangeni'),
-    THB: _('Thailand Baht'),
-    TJS: _('Tajikistan Somoni'),
-    TMT: _('Turkmenistan Manat'),
-    TND: _('Tunisia Dinar'),
-    TOP: _('Tonga Pa\'anga'),
-    TRY: _('Turkey Lira'),
-    TTD: _('Trinidad and Tobago Dollar'),
-    TVD: _('Tuvalu Dollar'),
-    TWD: _('Taiwan New Dollar'),
-    TZS: _('Tanzania Shilling'),
-    UAH: _('Ukraine Hryvna'),
-    UGX: _('Uganda Shilling'),
-    USD: _('United States Dollar'),
-    UYU: _('Uruguay Peso'),
-    UZS: _('Uzbekistan Som'),
-    VEF: _('Venezuela Bolivar'),
-    VND: _('Viet Nam Dong'),
-    VUV: _('Vanuatu Vat'),
-    WST: _('Samoa Tala'),
-    XAF: _('Communauté Financière Africaine (BEAC) CFA Franc BEAC'),
-    XCD: _('East Caribbean Dollar'),
-    XDR: _('International Monetary Fund (IMF) Special Drawing Rights'),
-    XOF: _('Communauté Financière Africaine (BCEAO) Franc'),
-    XPF: _('Comptoirs Français du Pacifique (CFP) Franc'),
-    YER: _('Yemen Rial'),
-    ZAR: _('South Africa Rand'),
-    ZMW: _('Zambia Kwacha'),
-    ZWD: _('Zimbabwe Dollar'),
-}
+    'AED': 'United Arab Emirates Dirham',
+    'AFN': 'Afghan Afghani',
+    'ALL': 'Albanian Lek',
+    'AMD': 'Armenian Dram',
+    'ANG': 'Netherlands Antillean Guilder',
+    'AOA': 'Angolan Kwanza',
+    'ARS': 'Argentine Peso',
+    'AUD': 'Australian Dollar',
+    'AWG': 'Aruban Florin',
+    'AZN': 'Azerbaijani Manat',
+    'BAM': 'Bosnia-Herzegovina Convertible Mark',
+    'BBD': 'Barbadian Dollar',
+    'BDT': 'Bangladeshi Taka',
+    'BGN': 'Bulgarian Lev',
+    'BHD': 'Bahraini Dinar',
+    'BIF': 'Burundian Franc',
+    'BMD': 'Bermudan Dollar',
+    'BND': 'Brunei Dollar',
+    'BOB': 'Bolivian Boliviano',
+    'BRL': 'Brazilian Real',
+    'BSD': 'Bahamian Dollar',
+    'BTC': 'Bitcoin',
+    'BTN': 'Bhutanese Ngultrum',
+    'BWP': 'Botswanan Pula',
+    'BYR': 'Belarusian Ruble',
+    'BZD': 'Belize Dollar',
+    'CAD': 'Canadian Dollar',
+    'CDF': 'Congolese Franc',
+    'CHF': 'Swiss Franc',
+    'CLF': 'Chilean Unit of Account (UF)',
+    'CLP': 'Chilean Peso',
+    'CNY': 'Chinese Yuan',
+    'COP': 'Colombian Peso',
+    'CRC': 'Costa Rican Col\xf3n',
+    'CUP': 'Cuban Peso',
+    'CVE': 'Cape Verdean Escudo',
+    'CZK': 'Czech Republic Koruna',
+    'DJF': 'Djiboutian Franc',
+    'DKK': 'Danish Krone',
+    'DOP': 'Dominican Peso',
+    'DZD': 'Algerian Dinar',
+    'EEK': 'Estonian Kroon',
+    'EGP': 'Egyptian Pound',
+    'ERN': 'Eritrean Nakfa',
+    'ETB': 'Ethiopian Birr',
+    'EUR': 'Euro',
+    'FJD': 'Fijian Dollar',
+    'FKP': 'Falkland Islands Pound',
+    'GBP': 'British Pound Sterling',
+    'GEL': 'Georgian Lari',
+    'GGP': 'Guernsey Pound',
+    'GHS': 'Ghanaian Cedi',
+    'GIP': 'Gibraltar Pound',
+    'GMD': 'Gambian Dalasi',
+    'GNF': 'Guinean Franc',
+    'GTQ': 'Guatemalan Quetzal',
+    'GYD': 'Guyanaese Dollar',
+    'HKD': 'Hong Kong Dollar',
+    'HNL': 'Honduran Lempira',
+    'HRK': 'Croatian Kuna',
+    'HTG': 'Haitian Gourde',
+    'HUF': 'Hungarian Forint',
+    'IDR': 'Indonesian Rupiah',
+    'ILS': 'Israeli New Sheqel',
+    'IMP': 'Manx pound',
+    'INR': 'Indian Rupee',
+    'IQD': 'Iraqi Dinar',
+    'IRR': 'Iranian Rial',
+    'ISK': 'Icelandic Kr\xf3na',
+    'JEP': 'Jersey Pound',
+    'JMD': 'Jamaican Dollar',
+    'JOD': 'Jordanian Dinar',
+    'JPY': 'Japanese Yen',
+    'KES': 'Kenyan Shilling',
+    'KGS': 'Kyrgystani Som',
+    'KHR': 'Cambodian Riel',
+    'KMF': 'Comorian Franc',
+    'KPW': 'North Korean Won',
+    'KRW': 'South Korean Won',
+    'KWD': 'Kuwaiti Dinar',
+    'KYD': 'Cayman Islands Dollar',
+    'KZT': 'Kazakhstani Tenge',
+    'LAK': 'Laotian Kip',
+    'LBP': 'Lebanese Pound',
+    'LKR': 'Sri Lankan Rupee',
+    'LRD': 'Liberian Dollar',
+    'LSL': 'Lesotho Loti',
+    'LTL': 'Lithuanian Litas',
+    'LVL': 'Latvian Lats',
+    'LYD': 'Libyan Dinar',
+    'MAD': 'Moroccan Dirham',
+    'MDL': 'Moldovan Le',
+    'MGA': 'Malagasy Ariary',
+    'MKD': 'Macedonian Denar',
+    'MMK': 'Myanma Kyat',
+    'MNT': 'Mongolian Tugrik',
+    'MOP': 'Macanese Pataca',
+    'MRO': 'Mauritanian Ouguiya',
+    'MTL': 'Maltese Lira',
+    'MUR': 'Mauritian Rupee',
+    'MVR': 'Maldivian Rufiyaa',
+    'MWK': 'Malawian Kwacha',
+    'MXN': 'Mexican Peso',
+    'MYR': 'Malaysian Ringgit',
+    'MZN': 'Mozambican Metical',
+    'NAD': 'Namibian Dollar',
+    'NGN': 'Nigerian Naira',
+    'NIO': 'Nicaraguan C\xf3rdoba',
+    'NOK': 'Norwegian Krone',
+    'NPR': 'Nepalese Rupee',
+    'NZD': 'New Zealand Dollar',
+    'OMR': 'Omani Rial',
+    'PAB': 'Panamanian Balboa',
+    'PEN': 'Peruvian Nuevo Sol',
+    'PGK': 'Papua New Guinean Kina',
+    'PHP': 'Philippine Peso',
+    'PKR': 'Pakistani Rupee',
+    'PLN': 'Polish Zloty',
+    'PYG': 'Paraguayan Guarani',
+    'QAR': 'Qatari Rial',
+    'RON': 'Romanian Le',
+    'RSD': 'Serbian Dinar',
+    'RUB': 'Russian Ruble',
+    'RWF': 'Rwandan Franc',
+    'SAR': 'Saudi Riyal',
+    'SBD': 'Solomon Islands Dollar',
+    'SCR': 'Seychellois Rupee',
+    'SDG': 'Sudanese Pound',
+    'SEK': 'Swedish Krona',
+    'SGD': 'Singapore Dollar',
+    'SHP': 'Saint Helena Pound',
+    'SLL': 'Sierra Leonean Leone',
+    'SOS': 'Somali Shilling',
+    'SRD': 'Surinamese Dollar',
+    'STD': 'S\xe3o Tom\xe9 and Pr\xedncipe Dobra',
+    'SVC': 'Salvadoran Col\xf3n',
+    'SYP': 'Syrian Pound',
+    'SZL': 'Swazi Lilangeni',
+    'THB': 'Thai Baht',
+    'TJS': 'Tajikistani Somoni',
+    'TMT': 'Turkmenistani Manat',
+    'TND': 'Tunisian Dinar',
+    'TOP': 'Tongan Pa\u02bbanga',
+    'TRY': 'Turkish Lira',
+    'TTD': 'Trinidad and Tobago Dollar',
+    'TWD': 'New Taiwan Dollar',
+    'TZS': 'Tanzanian Shilling',
+    'UAH': 'Ukrainian Hryvnia',
+    'UGX': 'Ugandan Shilling',
+    'USD': 'United States Dollar',
+    'UY': 'Uruguayan Peso',
+    'UZS': 'Uzbekistan Som',
+    'VEF': 'Venezuelan Bol\xedvar Fuerte',
+    'VND': 'Vietnamese Dong',
+    'VUV': 'Vanuatu Vat',
+    'WST': 'Samoan Tala',
+    'XAF': 'CFA Franc BEAC',
+    'XAG': 'Silver (troy ounce)',
+    'XA': 'Gold (troy ounce)',
+    'XCD': 'East Caribbean Dollar',
+    'XDR': 'Special Drawing Rights',
+    'XOF': 'CFA Franc BCEAO',
+    'XPF': 'CFP Franc',
+    'YER': 'Yemeni Rial',
+    'ZAR': 'South African Rand',
+    'ZMK': 'Zambian Kwacha (pre-2013)',
+    'ZMW': 'Zambian Kwacha',
+    'ZWL': 'Zimbabwean Dollar'}
