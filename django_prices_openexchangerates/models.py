af7762be76945f267801d1a9fe09b46a2df141dd
Artur Smęt <artur.smet@mirumee.com>
2016-10-18 14:24:24 +0200

    Extend currency rate field, to fit all currency rates

--- django_prices_openexchangerates/models.py
+++ django_prices_openexchangerates/models.py
@@ -43,7 +43,7 @@ class ConversionRate(models.Model):
         choices=CURRENCIES.items(), unique=True)
 
     rate = models.DecimalField(
-        _('Conversion rate'), max_digits=13, decimal_places=6)
+        _('Conversion rate'), max_digits=20, decimal_places=12)
 
     modified_at = models.DateTimeField(auto_now=True)
 
