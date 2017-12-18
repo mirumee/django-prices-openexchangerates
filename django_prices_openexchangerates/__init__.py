98467181780cb6f1c8e1d54b3869c31dc19bdfe5
michal <michal.sipa@mirumee.com>
2017-12-18 15:58:29 +0100

    CurrencyConversion should be able to apply conversion on Amount and Price objects
    Add clause in CurrencyConversion.apply to work correctly with Price objects.
    Add tests.

--- django_prices_openexchangerates/__init__.py
+++ django_prices_openexchangerates/__init__.py
@@ -32,6 +32,11 @@ class CurrencyConversion(object):
         if isinstance(price_obj, Amount):
             return Amount(
                 price_obj.value * self.rate, currency=self.to_currency)
+        if isinstance(price_obj, Price):
+            return Price(Amount(price_obj.net.value * self.rate,
+                         currency=self.to_currency),
+                         Amount(price_obj.gross.value * self.rate,
+                         currency=self.to_currency))
 
 
 def get_conversion_rate(currency):
