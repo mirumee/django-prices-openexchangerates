7a5d7af7f950991cb0314ef8c03487e75ba9c59c
Rafał Pitoń <rafal@mirumee.com>
2018-01-31 14:38:41 +0100

    Remove remaining future imports

--- django_prices_openexchangerates/tasks.py
+++ django_prices_openexchangerates/tasks.py
@@ -1,5 +1,3 @@
-from __future__ import division
-
 from decimal import Decimal
 
 import requests
--- tests/test_openexchangerates.py
+++ tests/test_openexchangerates.py
@@ -1,6 +1,4 @@
 # coding: utf-8
-from __future__ import unicode_literals
-
 import functools
 from decimal import Decimal
 
