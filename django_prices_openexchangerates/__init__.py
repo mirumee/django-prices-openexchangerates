5531f4a0b3c1a2cffd7b970554aa9e2e505cf671
Rafał Pitoń <rafal@mirumee.com>
2018-01-31 14:32:10 +0100

    Remove unicode literals imports

--- django_prices_openexchangerates/__init__.py
+++ django_prices_openexchangerates/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals
-
 import operator
 from decimal import Decimal
 
--- django_prices_openexchangerates/currencies.py
+++ django_prices_openexchangerates/currencies.py
@@ -1,5 +1,4 @@
 # coding: utf-8
-from __future__ import unicode_literals
 from django.utils.translation import ugettext_lazy as _
 
 # List of currencies supported by OpenExchange
