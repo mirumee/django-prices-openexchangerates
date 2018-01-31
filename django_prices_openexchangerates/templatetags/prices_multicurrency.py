506fdbf5a9e831a647cb95bca86c548463f8d289
Rafał Pitoń <rafal@mirumee.com>
2018-01-31 16:36:32 +0100

    Add types annotations to template tags

--- django_prices_openexchangerates/templatetags/prices_multicurrency.py
+++ django_prices_openexchangerates/templatetags/prices_multicurrency.py
@@ -1,19 +1,20 @@
 from django.template import Library
 
 from django_prices.templatetags import prices
+from prices import Money
 
-from .. import exchange_currency
+from .. import Exchangeable, exchange_currency
 
 register = Library()
 
 
 @register.filter
-def in_currency(base, currency, **kwargs):
+def in_currency(base: Exchangeable, currency: str, **kwargs) -> Exchangeable:
     converted_base = exchange_currency(base, currency)
     converted_base = converted_base.quantize('.01')
     return converted_base
 
 
 @register.filter
-def amount(obj):
+def amount(obj: Money) -> str:
     return prices.amount(obj)
--- django_prices_openexchangerates/templatetags/prices_multicurrency_i18n.py
+++ django_prices_openexchangerates/templatetags/prices_multicurrency_i18n.py
@@ -1,21 +1,26 @@
+from typing import Callable
+
 from django.template import Library
-
 from django_prices.templatetags import prices_i18n
+from prices import Money
 
-from .. import exchange_currency
+from .. import Exchangeable, exchange_currency
+
+Discount = Callable[[Exchangeable], Exchangeable]
 
 register = Library()
 
 
 @register.filter
-def in_currency(base, currency, **kwargs):
+def in_currency(base: Exchangeable, currency: str, **kwargs) -> Exchangeable:
     converted_base = exchange_currency(base, currency)
     converted_base = converted_base.quantize('.01')
     return converted_base
 
 
 @register.simple_tag
-def discount_amount_in_currency(base, discount, currency):
+def discount_amount_in_currency(
+        base: Exchangeable, discount: Discount, currency: str) -> Exchangeable:
     discount_amount = discount(base)
     converted_base = exchange_currency(base, to_currency=currency)
     discount_amount = exchange_currency(discount_amount, to_currency=currency)
@@ -23,5 +28,5 @@ def discount_amount_in_currency(base, discount, currency):
 
 
 @register.filter
-def amount(base, format='text'):
+def amount(base: Money, format='text') -> str:
     return prices_i18n.amount(base, format=format)
